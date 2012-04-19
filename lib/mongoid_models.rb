76d3693eab5e86d1107c3e87c03aa385fed254b6
David Vallance <davevallance@gmail.com>
2012-04-18 21:34:48 -0400

    Renaming production to deveopment in my mongoid.yml file and the associated irb_helper method.

--- lib/mongoid_models.rb
+++ lib/mongoid_models.rb
@@ -5,7 +5,7 @@ require 'bcrypt'
 
 def irb_helper
   require 'ruby-debug'
-  ENV['RACK_ENV'] = 'production'
+  ENV['RACK_ENV'] = 'development'
   require 'mongoid_models'
   include MongoidModels
   Mongoid.load!("test/mongoid.yml")
